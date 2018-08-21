214b6425ebce1576e53da72f0d27b6ed0d54d1bd
ahsan <ahsanalilodhi@gmail.com>
2018-08-21 20:39:02 +0200

    extension activates only on webpages with atleast 1 video

--- background.js
+++ background.js
@@ -1,19 +1,24 @@
-// Copyright 2018 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
+// 
+// 
+// 
 
 'use strict';
+// If the page has atleast 1 video tag
+var condition = new chrome.declarativeContent.PageStateMatcher({css: ["video"]}); 
 
-chrome.runtime.onInstalled.addListener(function() {
-  chrome.storage.sync.set({color: '#3aa757'}, function() {
-    console.log('The color is green.');
+// Then show the pageAction on this page
+var rule = {
+  conditions: [condition],
+  actions: [ new chrome.declarativeContent.ShowPageAction() ]
+};
+
+function init(){
+  chrome.runtime.onInstalled.addListener(function(details) {
+    chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+      chrome.declarativeContent.onPageChanged.addRules([rule]);
+    });
   });
-  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-    chrome.declarativeContent.onPageChanged.addRules([{
-      conditions: [new chrome.declarativeContent.PageStateMatcher({
-        pageUrl: {hostEquals: 'developer.chrome.com'},
-      })],
-      actions: [new chrome.declarativeContent.ShowPageAction()]
-    }]);
-  });
-});
+}
+
+// Start the app
+init();
