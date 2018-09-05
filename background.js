e10be399e254ff130c2dba9977e1f4f3d17987df
ahsanalilodhi <ahsanalilodhi@gmail.com>
2018-09-05 16:04:26 +0200

    message passing via chromoe runtime - 1

--- background.js
+++ background.js
@@ -12,12 +12,28 @@ var rule = {
   actions: [ new chrome.declarativeContent.ShowPageAction() ]
 };
 
+function initAuthListener(){
+  chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+      console.log(sender.tab ?
+                  "from a content script:" + sender.tab.url :
+                  "from the extension");
+      if (request.api_info != null){
+        chrome.storage.sync.set({key: value}, function() {
+          console.log('Value is set to ' + value);
+        });
+        sendResponse({farewell: "api_info saved"});
+      }
+      sendResponse({farewell: "api_info saving failed"});
+    });
+}
 function init(){
   chrome.runtime.onInstalled.addListener(function(details) {
     chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
       chrome.declarativeContent.onPageChanged.addRules([rule]);
     });
   });
+  initAuthListener();
 }
 
 // Start the app
